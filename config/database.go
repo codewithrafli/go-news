c2fe1cd54d621db87060cd6d1738cbcec9fa80d0
xrafffcode <yusfiwawan2@gmail.com>
2025-11-05 23:01:58 +0700

    Update .env configuration for PostgreSQL connection and add .env.example and .gitignore files; enhance database connection handling in config package

---
+++ config/database.go
@@ -0,0 +1,50 @@
+package config
+
+import (
+	"fmt"
+
+	"github.com/rs/zerolog/log"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+// Struct untuk koneksi ke database PostgreSQL
+type Postgres struct {
+	DB *gorm.DB
+}
+
+// Fungsi untuk mengkoneksikan ke database PostgreSQL
+func (cfg Config) ConnectionPostgres() (*Postgres, error) {
+
+	// Membuat string koneksi ke database PostgreSQL
+	dbConnString := fmt.Sprintf("postgres://%s:%s@%s:%d/%s",
+		cfg.PsqlDB.User,
+		cfg.PsqlDB.Password,
+		cfg.PsqlDB.Host,
+		cfg.PsqlDB.Port,
+		cfg.PsqlDB.DBName,
+	)
+
+	// Membuat koneksi ke database PostgreSQL
+	db, err := gorm.Open(postgres.Open(dbConnString), &gorm.Config{})
+
+	// Jika gagal mengkoneksikan ke database PostgreSQL
+	if err != nil {
+		log.Error().Err(err).Msg("Failed to connect to database")
+		return nil, err
+	}
+
+	// Mendapatkan koneksi ke database PostgreSQL
+	sqlDB, err := db.DB()
+	if err != nil {
+		log.Error().Err(err).Msg("Failed to get database connection")
+		return nil, err
+	}
+
+	// Mengatur jumlah maksimal koneksi terbuka dan idle
+	sqlDB.SetMaxOpenConns(cfg.PsqlDB.DBMaxOpen)
+	sqlDB.SetMaxIdleConns(cfg.PsqlDB.DBMaxIdle)
+
+	// Mengembalikan koneksi ke database PostgreSQL
+	return &Postgres{DB: db}, nil
+}
